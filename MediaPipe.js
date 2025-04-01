7d935b9d7ddbb764bd29ccfa58c3fc5ac2af3828
unknown <nyzxyn@gmail.com>
2025-04-01 13:35:09 +0800

    add pose detect

--- MediaPipe.js
+++ MediaPipe.js
@@ -44,7 +44,7 @@ async function setupMediaPipe() {
 
     const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
-            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy.task",
+            modelAssetPath: "./models/MediaPipe/pose_landmarker_lite.task",
             delegate: "GPU",
         },
         runningMode: "VIDEO",
