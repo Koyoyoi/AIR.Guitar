6d4d254c891d8a8e2ea803fc7041405a5c98028b
unknown <nyzxyn@gmail.com>
2025-04-07 09:55:19 +0800

    update mediapipe.js

---
+++ MediaPipe.js
@@ -0,0 +1,86 @@
+import { HandLandmarker, PoseLandmarker, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest";
+import { drawingUtils, handData, poseData } from './main.js'
+
+let handLandmarker, poseLandmarker;
+
+// 設置 MediaPipe
+export async function setupMediaPipe() {
+    const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+
+    handLandmarker = await HandLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "./models/MediaPipe/hand_landmarker.task",
+            delegate: "GPU"
+        },
+        runningMode: "VIDEO",
+        numHands: 2
+    });
+
+    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "./models/MediaPipe/pose_landmarker_lite.task",
+            delegate: "GPU",
+        },
+        runningMode: "VIDEO",
+        numPoses: 1
+    });
+   
+}
+
+export async function detectHand(video) {
+    if (!handLandmarker) return;
+
+    let data = handLandmarker.detectForVideo(video, performance.now())
+
+    // 如果偵測到手部標誌點，則繪製標記
+    if (data.landmarks) {
+        for (const landmarks of data.landmarks) {
+            // 畫出手部關節連線
+            drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, { color: "green", lineWidth: 3 });
+            // 畫出手指關鍵點
+            drawingUtils.drawLandmarks(landmarks, { color: "red", radius: 5 });
+        }
+    }
+
+    const handPoints = data.landmarks;
+    const handednesses = data.handednesses;
+
+    for (let i = 0; i < handednesses.length; i++) {
+        let points = [];
+        let left_or_right = String(handednesses[i][0].categoryName);
+        for (let p of handPoints[i]) {
+            p = [p.x * video.videoWidth, p.y * video.videoHeight, p.z];
+            points.push(p);
+        }
+        handData[left_or_right] = points;
+    }
+
+}
+
+export async function detectPose(video) {
+    if (!poseLandmarker) return;
+
+    let data = poseLandmarker.detectForVideo(video, performance.now());
+
+    // 如果偵測到身體標誌點，則繪製標記
+        /*
+        if (data.landmarks) {
+            for (const landmarks of data.landmarks) {
+                // 畫出身體關節連線
+                drawingUtils.drawConnectors(landmarks, PoseLandmarker.POSE_CONNECTIONS, { color: "green", lineWidth: 3 });
+                // 畫出身體關鍵點
+                drawingUtils.drawLandmarks(landmarks, { color: "red", radius: 5 });
+            }
+        }*/
+    
+    
+    const posePoints = data.landmarks;
+
+    for (let p of posePoints[0]){
+        p = [p.x * video.videoWidth, p.y * video.videoHeight, p.z];
+        poseData.push(p)
+    }
+
+}
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { HandLandmarker, PoseLandmarker, FilesetResolver, DrawingUtils } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest";
+import { setupMediaPipe, detectHand, detectPose } from "./MediaPipe.js";
 import { compute, fingerPlay } from "./handCompute.js";
 import { load_SVM_Model, predict } from "./SVM.js";
 import { initMIDI, plucking, buildGuitarChord } from "./MIDI.js";
+import { DrawingUtils } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest"
 
 // 宣告全域變數
+export let drawingUtils;
+export let handData = { "Left": [], "Right": [] }, poseData = [];
 let video, canvas, ctx;
-let handLandmarker, poseLandmarker, drawingUtils;
-let handData = { "Left": [], "Right": [] }, poseData = [];
 let gesture = '', prevGesture = '';
 let pluck = [], prevPluck = [];
 let capo = 0;
@@ -20,94 +21,31 @@ async function setupCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
 
+    canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    drawingUtils = new DrawingUtils(ctx);
+
     return new Promise((resolve) => {
         video.onloadedmetadata = () => {
+            // 設定 canvas 大小，保證有正確尺寸
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+
             video.play();
             resolve(video);
         };
     });
 }
 
-// 設置 MediaPipe
-async function setupMediaPipe() {
-    const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-    );
 
-    handLandmarker = await HandLandmarker.createFromOptions(vision, {
-        baseOptions: {
-            modelAssetPath: "./models/MediaPipe/hand_landmarker.task",
-            delegate: "GPU"
-        },
-        runningMode: "VIDEO",
-        numHands: 2
-    });
-
-    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-        baseOptions: {
-            modelAssetPath: "./models/MediaPipe/pose_landmarker_lite.task",
-            delegate: "GPU",
-        },
-        runningMode: "VIDEO",
-        numPoses: 1
-    });
-}
-
-// 設置畫布（Canvas）以便繪製手部偵測結果
-function setupCanvas() {
-    canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-    ctx = canvas.getContext("2d");
-    drawingUtils = new DrawingUtils(ctx);
-
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-}
-
-// 偵測手部並繪製標記
 async function detect() {
-    if (!handLandmarker && !poseLandmarker) return;
-
-    let hand = handLandmarker.detectForVideo(video, performance.now());
-    let pose = poseLandmarker.detectForVideo(video, performance.now());
-
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // 如果偵測到手部標誌點，則繪製標記
-    if (hand.landmarks) {
-        for (const landmarks of hand.landmarks) {
-            // 畫出手部關節連線
-            drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, { color: "green", lineWidth: 3 });
-            // 畫出手指關鍵點
-            drawingUtils.drawLandmarks(landmarks, { color: "red", radius: 5 });
-        }
-    }
-    // 如果偵測到身體標誌點，則繪製標記
-    /*
-    if (pose.landmarks) {
-        for (const landmarks of pose.landmarks) {
-            // 畫出身體關節連線
-            drawingUtils.drawConnectors(landmarks, PoseLandmarker.POSE_CONNECTIONS, { color: "green", lineWidth: 3 });
-            // 畫出身體關鍵點
-            drawingUtils.drawLandmarks(landmarks, { color: "red", radius: 5 });
-        }
-    }*/
-
-    const handPoints = hand.landmarks;
-    const handednesses = hand.handednesses;
-    const posePoints = pose.landmarks;
-
-    for (let i = 0; i < handednesses.length; i++) {
-        let points = [];
-        let left_or_right = String(handednesses[i][0].categoryName);
-        for (let p of handPoints[i]) {
-            p = [p.x * video.videoWidth, p.y * video.videoHeight, p.z];
-            points.push(p);
-        }
-        handData[left_or_right] = points;
-    }
-  
+    await detectHand(video);
+    await detectPose(video)
 
     // Left Hand
     if (handData['Left'].length !== 0) {
@@ -155,7 +93,6 @@ async function main() {
     await setupCamera(); // 啟動攝影機
     await initMIDI();
     buildGuitarChord('C');
-    setupCanvas(); // 設置畫布
     detect(); // 啟動手部偵測
 }
 
