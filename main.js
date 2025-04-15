e79f0b1d5da6d10830c9b686de3b77fb4d85cbf0
unknown <nyzxyn@gmail.com>
2025-04-15 16:38:00 +0800

    add capo controll

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let armAngles = [];
 let gesture = '', prevGesture = '';
 let pluck = [], prevPluck = [];
 let action = '', prevAction = '';
-let capo = 0;
+let capo = 0, timeCnt = 0;
 
 // resize 函數
 function resizeCanvasAndVideo() {
@@ -43,8 +43,8 @@ async function setupCamera() {
 
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
-            width: { ideal: 1280},
-            height: { ideal: 720}
+            width: { ideal: 1280 },
+            height: { ideal: 720 }
         }
     });
 
@@ -86,13 +86,13 @@ async function detect() {
     // Left Hand
     if (handData['Left'].length != 0) {
         let parameters = compute(handData['Left']);
-        gesture = await predict(parameters)
+        gesture = await predict(parameters);
         if (prevGesture != gesture) {
             console.log(gesture);
             prevGesture = gesture;
             buildGuitarChord(gesture);
         }
-        drawGesture(gesture)
+        drawGesture(gesture);
     }
 
     // Right Hand
@@ -115,7 +115,7 @@ async function detect() {
 
     // Strumming Control
     if (poseData[12] != undefined && poseData[14] != undefined && poseData[16] != undefined) {
-        let angle = vectorAngle(vectorCompute(poseData[12], poseData[14]), vectorCompute(poseData[16], poseData[14]))
+        let angle = vectorAngle(vectorCompute(poseData[12], poseData[14]), vectorCompute(poseData[16], poseData[14]));
         armAngles.push(Math.round(angle));
         let position = poseData[16][0] - poseData[12][0];
 
@@ -145,10 +145,24 @@ async function detect() {
         }
     }
 
+    // Capo control 每1秒執行一次
+    if (poseData.length > 0 && timeCnt >= 30) { 
+        if (poseData[15][1] < poseData[0][1]) {
+            capo = Math.max(-12, capo - 1);
+        } else if (poseData[16][1] < poseData[0][1]) {
+            capo = Math.min(12, capo + 1);
+        }
+        console.log("Capo:", capo);
+        timeCnt = 0;
+    }
+    timeCnt += 1;
+
+    // 重置 handData 和 poseData
     handData['Left'] = [];
     handData['Right'] = [];
     poseData = [];
 
+    // 使用 requestAnimationFrame 確保無限循環
     requestAnimationFrame(detect);
 }
 
