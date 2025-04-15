832eb7137a6e2321a1b46b7c66f3b329fdf69f4e
unknown <nyzxyn@gmail.com>
2025-04-15 16:41:58 +0800

    update draw.js

--- draw.js
+++ draw.js
@@ -13,14 +13,14 @@ export function drawGesture(gesture) {
 
 }
 
-export function drawCapo() {
+export function drawCapo(capo) {
     // 設定字型與顏色
     ctx.font = "100px Arial";
     ctx.fillStyle = "#00AA90";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
     // 畫出 capo 文字（右上角）
-    
+
     ctx.textAlign = "right";  // 設為右對齊
     ctx.fillText(`Capo: ${capo}`, canvas.width - 50, 50);  // 顯示 capo，距離右邊 50px，並且在上方
 
