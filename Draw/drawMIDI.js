f84c4578b90ea4bf5bed2791bc1ab91e0db4bb81
unknown <nyzxyn@gmail.com>
2025-05-04 13:53:17 +0800

    update midi animation

--- Draw/drawMIDI.js
+++ Draw/drawMIDI.js
@@ -7,21 +7,19 @@ export async function draw_midiAnimation() {
         console.warn("⚠️ 沒有 MIDI 音符可顯示！");
         return;
     }
-    console.log(video.videoHeight, canvas.height)
+
+    const startTime = performance.now(); // 動畫起始時間
+    const pixelsPerSecond = 100; // 控制滑動速度
 
     const notes = noteSequence.notes.map(note => ({
         pitch: note.pitch,
         start: note.startTime,
         end: note.endTime,
         x: canvas.width, // 起始 X 座標（畫面右側）
-        y: -50,  // 初始 Y 座標，畫面頂端外
-        shown: false,
+        y: mapRange(note.pitch, 24, 96, video.videoHeight, 0),
+        w: (note.endTime - note.startTime) * pixelsPerSecond,
+        h: 9
     }));
-
-    const startTime = performance.now(); // 動畫起始時間
-    const pixelsPerSecond = 100; // 控制滑動速度
-    const videoHeight = video.videoHeight; // 使用影片的高度
-    const noteHeight = 9; // 音符的高度
 
     function drawFrame(now) {
         const elapsed = (now - startTime) / 1000; // 秒為單位
@@ -32,19 +30,12 @@ export async function draw_midiAnimation() {
                 // 計算 X 位置，從畫布右側開始移動
                 note.x = canvas.width - (elapsed - note.start) * pixelsPerSecond; // X 位置向左移動
 
-                // 計算 Y 位置，根據音高來設置
-                const y = mapRange(note.pitch, 24, 96, 720, 0); // 使用音高計算 Y 位置
-
-                // 計算音符寬度（根據持續時間設置）
-                const duration = note.end - note.start;
-                const width = duration * pixelsPerSecond;
-
                 // 使用 drawRect 函數繪製圓角矩形作為音符
                 const area = {
-                    x: note.x - width / 2,
-                    y: y,
-                    w: width,
-                    h: noteHeight
+                    x: note.x - note.w / 2,
+                    y: note.y,
+                    w: note.w,
+                    h: note.h
                 };
                 drawRect(area, 5, "#EE9A9A"); // 圓角半徑為 5，顏色為 #EE9A9A
             }
